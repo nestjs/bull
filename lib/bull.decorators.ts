2e94855c172463964639d914ee2051b87130c5ff
Frédéric Woelffel <woelffel.f@gmail.com>
2018-02-25 01:30:49 +0100

    feat: Make the InjectQueue name parameter optional

--- lib/bull.decorators.ts
+++ lib/bull.decorators.ts
@@ -1,6 +1,6 @@
 import { Inject } from '@nestjs/common';
 import { getQueueToken } from './bull.utils';
 
-export function InjectQueue(name: string): ParameterDecorator {
+export function InjectQueue(name?: string): ParameterDecorator {
   return Inject(getQueueToken(name));
 }
