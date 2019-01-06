914bc4e901d1689b0b11bbb47bff31935066dfab
Frédéric Woelffel <woelffel.f@gmail.com>
2019-01-06 19:36:52 +0100

    style: fix linting

--- lib/bull.types.ts
+++ lib/bull.types.ts
@@ -1,8 +1,12 @@
 import { DoneCallback, Job } from 'bull';
 
 export type BullQueueProcessor = BullQueueProcessorCallback | BullQueueAdvancedProcessor;
-export type BullQueueProcessorCallback = ((job: Job, done?: DoneCallback) => void)
-export type BullQueueAdvancedProcessor = { concurrency?: number, name?: string, callback: BullQueueProcessorCallback }
+export type BullQueueProcessorCallback = ((job: Job, done?: DoneCallback) => void);
+export interface BullQueueAdvancedProcessor {
+  concurrency?: number;
+  name?: string;
+  callback: BullQueueProcessorCallback;
+}
 
 export function isAdvancedProcessor(processor: BullQueueProcessor): processor is BullQueueAdvancedProcessor {
   return 'object' === typeof processor && !!processor.callback;
