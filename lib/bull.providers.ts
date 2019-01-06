fbb34b2f39f404746d660a2968678cba4a16700c
Frédéric Woelffel <woelffel.f@gmail.com>
2019-01-06 19:31:51 +0100

    fix: add test to check for processors array existence

--- lib/bull.providers.ts
+++ lib/bull.providers.ts
@@ -9,18 +9,20 @@ export function createQueues(options: BullModuleOptions[]): any[] {
     provide: getQueueToken(option.name),
     useFactory: (): Queue => {
       const queue: Queue = new Bull(option.name ? option.name : 'default', option.options);
-      option.processors.forEach((processor: BullQueueProcessor) => {
-        if (isAdvancedProcessor(processor)) {
-          const hasName = !!processor.name;
-          const hasConcurrency = !!processor.concurrency;
-          hasName && hasConcurrency ? queue.process(processor.name, processor.concurrency, processor.callback)
-          : hasName ? queue.process(processor.name, processor.callback)
-          : queue.process(processor.concurrency, processor.callback);
-        }
-        else {
-          queue.process(processor);
-        }
-      });
+      if (option.processors) {
+        option.processors.forEach((processor: BullQueueProcessor) => {
+          if (isAdvancedProcessor(processor)) {
+            const hasName = !!processor.name;
+            const hasConcurrency = !!processor.concurrency;
+            hasName && hasConcurrency ? queue.process(processor.name, processor.concurrency, processor.callback)
+              : hasName ? queue.process(processor.name, processor.callback)
+              : queue.process(processor.concurrency, processor.callback);
+          }
+          else {
+            queue.process(processor);
+          }
+        });
+      }
       return queue;
     },
   }));
