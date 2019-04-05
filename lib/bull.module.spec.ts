7d5a2b2aa659c47a23a6653120391333913a4cd3
Frederic Woelffel <frederic.woelffel@ihu-strasbourg.eu>
2019-04-05 20:03:59 +0200

    test: add test for single and multiple configurations

--- lib/bull.module.spec.ts
+++ lib/bull.module.spec.ts
@@ -4,36 +4,15 @@ import {Test, TestingModule} from '@nestjs/testing';
 
 describe('BullModule', () => {
   let module: TestingModule;
-
   describe('forRoot', () => {
-    const fakeProcessor = jest.fn();
-    beforeAll(async () => {
-      module = await Test.createTestingModule({
-        imports: [
-          BullModule.forRoot({
-            name: 'test',
-            processors: [fakeProcessor],
-          }),
-        ],
-      }).compile();
-    });
-    it('should inject the queue with the given name', () => {
-      const queue: Queue = module.get<Queue>(getQueueToken('test'));
-      expect(queue).toBeDefined();
-    });
-  });
-
-  describe('forRootAsync', () => {
-    describe('useFactory', () => {
+    describe('single configuration', () => {
       const fakeProcessor = jest.fn();
       beforeAll(async () => {
         module = await Test.createTestingModule({
           imports: [
-            BullModule.forRootAsync({
+            BullModule.forRoot({
               name: 'test',
-              useFactory: () => ({
-                processors: [fakeProcessor],
-              }),
+              processors: [fakeProcessor],
             }),
           ],
         }).compile();
@@ -43,26 +22,73 @@ describe('BullModule', () => {
         expect(queue).toBeDefined();
       });
     });
+    describe('multiple configuration', () => {
+      beforeAll(async () => {
+        module = await Test.createTestingModule({
+          imports: [
+            BullModule.forRoot([
+              { name: 'test1' },
+              { name: 'test2' },
+            ]),
+          ],
+        }).compile();
+      });
+      it('should inject the queue with name "test1"', () => {
+        const queue: Queue = module.get<Queue>(getQueueToken('test1'));
+        expect(queue).toBeDefined();
+      });
+      it('should inject the queue with name "test2"', () => {
+        const queue: Queue = module.get<Queue>(getQueueToken('test2'));
+        expect(queue).toBeDefined();
+      });
+    });
   });
-
-  describe('#13 Async configuration does not allow an array of BullModuleOptions[]', () => {
-    beforeAll(async () => {
-      module = await Test.createTestingModule({
-        imports: [
-          BullModule.forRootAsync([
-            { name: 'test1', useFactory: () => ({}) },
-            { name: 'test2', useFactory: () => ({}) },
-          ]),
-        ],
-      }).compile();
+  describe('forRootAsync', () => {
+    describe('single configuration', () => {
+      describe('useFactory', () => {
+        const fakeProcessor = jest.fn();
+        beforeAll(async () => {
+          module = await Test.createTestingModule({
+            imports: [
+              BullModule.forRootAsync({
+                name: 'test',
+                useFactory: () => ({
+                  processors: [fakeProcessor],
+                }),
+              }),
+            ],
+          }).compile();
+        });
+        it('should inject the queue with the given name', () => {
+          const queue: Queue = module.get<Queue>(getQueueToken('test'));
+          expect(queue).toBeDefined();
+        });
+        it('the injected queue should have the given processor', () => {
+          const queue: Queue = module.get<Queue>(getQueueToken('test'));
+        });
+      });
     });
-    it('should inject the queue with name "test1"', () => {
-      const queue: Queue = module.get<Queue>(getQueueToken('test1'));
-      expect(queue).toBeDefined();
-    });
-    it('should inject the queue with name "test2"', () => {
-      const queue: Queue = module.get<Queue>(getQueueToken('test2'));
-      expect(queue).toBeDefined();
+    describe('multiple configuration', () => {
+      describe('useFactory', () => {
+        beforeAll(async () => {
+          module = await Test.createTestingModule({
+            imports: [
+              BullModule.forRootAsync([
+                { name: 'test1', useFactory: () => ({}) },
+                { name: 'test2', useFactory: () => ({}) },
+              ]),
+            ],
+          }).compile();
+        });
+        it('should inject the queue with name "test1"', () => {
+          const queue: Queue = module.get<Queue>(getQueueToken('test1'));
+          expect(queue).toBeDefined();
+        });
+        it('should inject the queue with name "test2"', () => {
+          const queue: Queue = module.get<Queue>(getQueueToken('test2'));
+          expect(queue).toBeDefined();
+        });
+      });
     });
   });
 });
