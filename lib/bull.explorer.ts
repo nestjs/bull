0dc5d92d1282e47a91c4a326f18a1ec5f10e525a
Frederic Woelffel <frederic.woelffel@ihu-strasbourg.eu>
2019-05-03 07:52:34 +0200

    feat: add support for event listener decorator
    This commit also fixes the process decorator handling

--- lib/bull.explorer.ts
+++ lib/bull.explorer.ts
@@ -2,7 +2,7 @@ import {Injectable as InjectableDecorator, Type} from '@nestjs/common';
 import {ModulesContainer, ModuleRef} from '@nestjs/core';
 import {InstanceWrapper} from '@nestjs/core/injector/instance-wrapper';
 import {Module} from '@nestjs/core/injector/module';
-import {BULL_MODULE_QUEUE_LISTENER, BULL_MODULE_PROCESS} from './bull.constants';
+import {BULL_MODULE_QUEUE_LISTENER, BULL_MODULE_PROCESS, BULL_MODULE_ON_EVENT} from './bull.constants';
 import {Injectable} from '@nestjs/common/interfaces';
 import {MetadataScanner} from '@nestjs/core/metadata-scanner';
 import {getQueueToken} from './bull.utils';
@@ -21,11 +21,27 @@ export class BullExplorer {
         components.map((wrapper: InstanceWrapper) => {
             const { instance, metatype } = wrapper;
             const queue = BullExplorer.getBullQueue(this.moduleRef, BullExplorer.getClassMetadata(metatype).queueName);
-            const processorMethodKeys = BullExplorer.getProcessorMethods(instance);
-            for (const key of processorMethodKeys) {
-                const options = BullExplorer.getMethodMetadata(instance, key) || {};
-                queue.process(options.name, options.concurrency, instance[key].bind(instance));
-            }
+            new MetadataScanner()
+                .scanFromPrototype(
+                    instance,
+                    Object.getPrototypeOf(instance),
+                    (key: string) => {
+                        if (BullExplorer.isMethodDecoratedAsProcessor(instance, key)) {
+                            const options = BullExplorer.getProcessorMetadata(instance, key) || {};
+                            options.name && options.concurrency
+                                ? queue.process(options.name, options.concurrency, instance[key].bind(instance))
+                                : options.name
+                                ? queue.process(options.name, instance[key].bind(instance))
+                                : options.concurrency
+                                ? queue.process(options.concurrency, instance[key].bind(instance))
+                                : queue.process(instance[key].bind(instance));
+                        }
+                        else if (BullExplorer.isMethodDecoratedAsEventListener(instance, key)) {
+                            const options = BullExplorer.getEventListenerMetadata(instance, key) || {};
+                            queue.on(options.eventName, instance[key].bind(instance));
+                        }
+                    }
+                );
         });
     }
 
@@ -41,25 +57,20 @@ export class BullExplorer {
         return Reflect.hasMetadata(BULL_MODULE_PROCESS, instance, methodKey);
     }
 
-    private static getMethodMetadata(instance: Injectable, methodKey: string): any {
+    private static isMethodDecoratedAsEventListener(instance: Injectable, methodKey: string): boolean {
+        return Reflect.hasMetadata(BULL_MODULE_ON_EVENT, instance, methodKey);
+    }
+
+    private static getProcessorMetadata(instance: Injectable, methodKey: string): any {
         return Reflect.getMetadata(BULL_MODULE_PROCESS, instance, methodKey);
+    }
+
+    private static getEventListenerMetadata(instance: Injectable, methodKey: string): any {
+        return Reflect.getMetadata(BULL_MODULE_ON_EVENT, instance, methodKey);
     }
 
     private static getBullQueue(moduleRef: ModuleRef, queueName?: string): Queue {
         return moduleRef.get<Queue>(getQueueToken(queueName));
-    }
-
-    private static getProcessorMethods(instance: Injectable): string[] {
-        return new MetadataScanner()
-            .scanFromPrototype(
-                instance,
-                Object.getPrototypeOf(instance),
-                (key: string) => {
-                    return BullExplorer.isMethodDecoratedAsProcessor(instance, key)
-                        ? key
-                        : null;
-                }
-            );
     }
 
     private static getDecoratedComponents(modules: Module[]): InstanceWrapper<Injectable>[] {
