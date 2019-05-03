58270db8f63b007b712d9e3cb5b6fd74a4356897
Frederic Woelffel <frederic.woelffel@ihu-strasbourg.eu>
2019-05-03 12:42:26 +0200

    refactor: rewrite event decorators using the generic event decorator

--- lib/bull.decorators.ts
+++ lib/bull.decorators.ts
@@ -1,332 +1,81 @@
 import {Inject} from '@nestjs/common';
 import {getQueueToken} from './bull.utils';
-import {
-    BULL_MODULE_QUEUE_LISTENER,
-    BULL_MODULE_PROCESS,
-    BULL_MODULE_ON_ERROR,
-    BULL_MODULE_ON_WAITING,
-    BULL_MODULE_ON_ACTIVE,
-    BULL_MODULE_ON_STALLED,
-    BULL_MODULE_ON_PROGRESS,
-    BULL_MODULE_ON_COMPLETED,
-    BULL_MODULE_ON_FAILED,
-    BULL_MODULE_ON_PAUSED,
-    BULL_MODULE_ON_RESUMED,
-    BULL_MODULE_ON_CLEANED,
-    BULL_MODULE_ON_DRAINED,
-    BULL_MODULE_ON_REMOVED,
-    BULL_MODULE_ON_GLOBAL_ERROR,
-    BULL_MODULE_ON_GLOBAL_WAITING,
-    BULL_MODULE_ON_GLOBAL_ACTIVE,
-    BULL_MODULE_ON_GLOBAL_STALLED,
-    BULL_MODULE_ON_GLOBAL_PROGRESS,
-    BULL_MODULE_ON_GLOBAL_COMPLETED,
-    BULL_MODULE_ON_GLOBAL_FAILED,
-    BULL_MODULE_ON_GLOBAL_PAUSED,
-    BULL_MODULE_ON_GLOBAL_RESUMED,
-    BULL_MODULE_ON_GLOBAL_CLEANED,
-    BULL_MODULE_ON_GLOBAL_DRAINED,
-    BULL_MODULE_ON_GLOBAL_REMOVED,
-    BULL_MODULE_ON_EVENT
-} from './bull.constants';
+import {BULL_MODULE_QUEUE_LISTENER, BULL_MODULE_PROCESS, BULL_MODULE_ON_EVENT} from './bull.constants';
 import {BullQueueEvent} from './bull.types';
+import {BullQueueEvents, BullQueueGlobalEvents} from './bull.enums';
 
-export function InjectQueue(name?: string): ParameterDecorator {
-    return Inject(getQueueToken(name));
-}
+export const InjectQueue = (name?: string): ParameterDecorator => Inject(getQueueToken(name));
 
-export const QueueListener = (options?: { name?: string }): ClassDecorator => {
-    return (target: any) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_QUEUE_LISTENER,
-            options || {},
-            target
-        );
-    };
+export const QueueListener = (options?: { name?: string }): ClassDecorator => (target: any) => {
+    Reflect.defineMetadata(
+        BULL_MODULE_QUEUE_LISTENER,
+        options || {},
+        target
+    );
 };
 
-export const Process = (options?: { name?: string, concurrency?: number }) => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_PROCESS,
-            options,
-            target,
-            propertyName,
-        );
-    };
+export const Process = (options?: { name?: string, concurrency?: number }): MethodDecorator => (target: any, propertyName: string) => {
+    Reflect.defineMetadata(
+        BULL_MODULE_PROCESS,
+        options,
+        target,
+        propertyName,
+    );
 };
 
-export const OnEvent = (eventName: BullQueueEvent) => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_EVENT,
-            { eventName },
-            target,
-            propertyName
-        );
-    };
+export const OnEvent = (eventName: BullQueueEvent): MethodDecorator => (target: any, propertyName: string) => {
+    Reflect.defineMetadata(
+        BULL_MODULE_ON_EVENT,
+        { eventName },
+        target,
+        propertyName
+    );
 };
 
-export const OnError = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_ERROR,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnError = () => OnEvent(BullQueueEvents.ERROR);
 
-export const OnWaiting = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_WAITING,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnWaiting = () => OnEvent(BullQueueEvents.WAITING);
 
-export const OnActive = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_ACTIVE,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnActive = () => OnEvent(BullQueueEvents.ACTIVE);
 
-export const OnStalled = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_STALLED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnStalled = () => OnEvent(BullQueueEvents.STALLED);
 
-export const OnProgress = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_PROGRESS,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnProgress = () => OnEvent(BullQueueEvents.PROGRESS);
 
-export const OnCompleted = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_COMPLETED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnCompleted = () => OnEvent(BullQueueEvents.COMPLETED);
 
-export const OnFailed = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_FAILED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnFailed = () => OnEvent(BullQueueEvents.FAILED);
 
-export const OnPaused = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_PAUSED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnPaused = () => OnEvent(BullQueueEvents.PAUSED);
 
-export const OnResumed = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_RESUMED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnResumed = () => OnEvent(BullQueueEvents.RESUMED);
 
-export const OnCleaned = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_CLEANED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnCleaned = () => OnEvent(BullQueueEvents.CLEANED);
 
-export const OnDrained = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_DRAINED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnDrained = () => OnEvent(BullQueueEvents.DRAINED);
 
-export const OnRemoved = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_REMOVED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnRemoved = () => OnEvent(BullQueueEvents.REMOVED);
 
-export const OnGlobalError = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_ERROR,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalError = () => OnEvent(BullQueueGlobalEvents.ERROR);
 
-export const OnGlobalWaiting = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_WAITING,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalWaiting = () => OnEvent(BullQueueGlobalEvents.WAITING);
 
-export const OnGlobalActive = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_ACTIVE,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalActive = () => OnEvent(BullQueueGlobalEvents.ACTIVE);
 
-export const OnGlobalStalled = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_STALLED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalStalled = () => OnEvent(BullQueueGlobalEvents.STALLED);
 
-export const OnGlobalProgress = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_PROGRESS,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalProgress = () => OnEvent(BullQueueGlobalEvents.PROGRESS);
 
-export const OnGlobalCompleted = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_COMPLETED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalCompleted = () => OnEvent(BullQueueGlobalEvents.COMPLETED);
 
-export const OnGlobalFailed = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_FAILED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalFailed = () => OnEvent(BullQueueGlobalEvents.FAILED);
 
-export const OnGlobalPaused = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_PAUSED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalPaused = () => OnEvent(BullQueueGlobalEvents.PAUSED);
 
-export const OnGlobalResumed = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_RESUMED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalResumed = () => OnEvent(BullQueueGlobalEvents.RESUMED);
 
-export const OnGlobalCleaned = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_CLEANED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalCleaned = () => OnEvent(BullQueueGlobalEvents.CLEANED);
 
-export const OnGlobalDrained = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_DRAINED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalDrained = () => OnEvent(BullQueueGlobalEvents.DRAINED);
 
-export const OnGlobalRemoved = () => {
-    return (target: any, propertyName: string) => {
-        Reflect.defineMetadata(
-            BULL_MODULE_ON_GLOBAL_REMOVED,
-            null,
-            target,
-            propertyName,
-        );
-    };
-};
+export const OnGlobalRemoved = () => OnEvent(BullQueueGlobalEvents.REMOVED);
