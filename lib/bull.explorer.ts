2ab9c62f528f601198213a42231220eec5cfab5e
Frederic Woelffel <hello@fwoelffel.me>
2019-05-05 21:16:58 +0200

    fix: fix the @Queue() decorator
    This commit fixes an issue occuring when using named queue(s).
    
    Closes #70

--- lib/bull.explorer.ts
+++ lib/bull.explorer.ts
@@ -1,16 +1,12 @@
-import { Injectable as InjectableDecorator, Type } from '@nestjs/common';
-import { ModulesContainer, ModuleRef } from '@nestjs/core';
-import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
-import { Module } from '@nestjs/core/injector/module';
-import {
-  BULL_MODULE_QUEUE,
-  BULL_MODULE_QUEUE_PROCESS,
-  BULL_MODULE_ON_QUEUE_EVENT,
-} from './bull.constants';
-import { Injectable } from '@nestjs/common/interfaces';
-import { MetadataScanner } from '@nestjs/core/metadata-scanner';
-import { getQueueToken } from './bull.utils';
-import { Queue } from 'bull';
+import {Injectable as InjectableDecorator, Type} from '@nestjs/common';
+import {ModulesContainer, ModuleRef} from '@nestjs/core';
+import {InstanceWrapper} from '@nestjs/core/injector/instance-wrapper';
+import {Module} from '@nestjs/core/injector/module';
+import {BULL_MODULE_QUEUE, BULL_MODULE_QUEUE_PROCESS, BULL_MODULE_ON_QUEUE_EVENT,} from './bull.constants';
+import {Injectable} from '@nestjs/common/interfaces';
+import {MetadataScanner} from '@nestjs/core/metadata-scanner';
+import {getQueueToken} from './bull.utils';
+import {Queue} from 'bull';
 
 @InjectableDecorator()
 export class BullExplorer {
@@ -27,7 +23,7 @@ export class BullExplorer {
       const { instance, metatype } = wrapper;
       const queue = BullExplorer.getBullQueue(
         this.moduleRef,
-        BullExplorer.getClassMetadata(metatype).queueName,
+        BullExplorer.getClassMetadata(metatype).name,
       );
       new MetadataScanner().scanFromPrototype(
         instance,
@@ -38,15 +34,15 @@ export class BullExplorer {
               BullExplorer.getProcessorMetadata(instance, key) || {};
             options.name && options.concurrency
               ? queue.process(
-                  options.name,
-                  options.concurrency,
-                  instance[key].bind(instance),
-                )
+              options.name,
+              options.concurrency,
+              instance[key].bind(instance),
+              )
               : options.name
               ? queue.process(options.name, instance[key].bind(instance))
               : options.concurrency
-              ? queue.process(options.concurrency, instance[key].bind(instance))
-              : queue.process(instance[key].bind(instance));
+                ? queue.process(options.concurrency, instance[key].bind(instance))
+                : queue.process(instance[key].bind(instance));
           } else if (
             BullExplorer.isMethodDecoratedAsEventListener(instance, key)
           ) {
@@ -96,7 +92,17 @@ export class BullExplorer {
   }
 
   private static getBullQueue(moduleRef: ModuleRef, queueName?: string): Queue {
-    return moduleRef.get<Queue>(getQueueToken(queueName));
+    const queue = moduleRef.get<Queue>(getQueueToken(queueName));
+    if (!queue) {
+      throw new Error(
+        queueName
+          ? `No Queue was found with the given name (${queueName}). Check your configuration.`
+          : 'No Queue was found. Check your configuration.'
+      );
+    }
+    else {
+      return queue;
+    }
   }
 
   private static getDecoratedComponents(
