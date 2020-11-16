e7e3c9cb59f179f5b10de2986bab5e7fb9d99713
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-11-16 13:33:08 +0100

    lint(): disable boundary types rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   },
 };
