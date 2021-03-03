5fb722352212f21794d6287441b762cdf73179dc
Max Isom <hi@maxisom.me>
2021-03-02 21:51:24 -0500

    fix(@nestjs/bull) small fixes

--- lib/test/bull.providers.spec.ts
+++ lib/test/bull.providers.spec.ts
@@ -6,7 +6,7 @@ describe('Providers', () => {
   describe('createQueueProviders', () => {
     it("should use top-level queue name if it's not specified in factory options", async () => {
       const moduleOptions: BullModuleOptions = { name: 'top-level-queue-name' };
-      const factoryModuleOptions: BullModuleOptions = { redis: { port: 6380 } };
+      const factoryModuleOptions: BullModuleOptions = { connection: { port: 6380 } };
       const provider = bullProviders.createQueueProviders([
         moduleOptions,
       ])[0] as FactoryProvider;
