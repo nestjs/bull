eb8852094efe077ebb657646a73d192ae6481a0d
Ivan Nalon <ivannalon@gmail.com>
2024-06-12 21:04:11 -0300

    fix: bull processor interfaces

--- packages/bullmq/lib/interfaces/bull-processor.interfaces.ts
+++ packages/bullmq/lib/interfaces/bull-processor.interfaces.ts
@@ -1,14 +1,15 @@
+import { WorkerOptions } from 'bullmq';
 import {
   BullQueueProcessorCallback,
   BullQueueSeparateProcessor,
 } from '../bull.types';
 
-export interface BullQueueAdvancedProcessor {
+export interface BullQueueAdvancedProcessor extends WorkerOptions {
   concurrency?: number;
   callback: BullQueueProcessorCallback;
 }
 
-export interface BullQueueAdvancedSeparateProcessor {
+export interface BullQueueAdvancedSeparateProcessor extends WorkerOptions {
   concurrency?: number;
   path: BullQueueSeparateProcessor;
   useWorkerThreads?: boolean;
